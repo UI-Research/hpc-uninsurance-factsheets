e978f7df84f8d228954f64af7ab42de0152b1927
Menezes <mmenezes@aharris-macbookpro.urban.org>
2019-05-31 14:23:09 -0400

    merged page 1 (Michelle) and page 2 (Leah)

--- iterate.R
+++ iterate.R
@@ -2,20 +2,39 @@ library(rmarkdown)
 library(stringr)
 library(tidyverse)
 
-#reading in the tables
-rm("table1", "table", "statement")
-table1 <- read.csv(file="/Users/mmenezes/Projects/hpc-uninsurance-factsheets/Tables/StateFactSheetData.csv", header=TRUE, sep=",")
+table1 <- read.csv(file="Tables/StateFactSheetData_521.csv", header=TRUE, sep=",")
+datdict <- read.csv(file="Tables/State Uninsurance WRA Data Dictionary.csv", header=TRUE, sep=",")
 
+table1_smallstate <- table1 %>%
+  filter(small==1)
+
+table1_largestate <- table1 %>%
+  filter(small==0)
 # create an index
-#index <- c("Aberdeen",  "Biloxi", "Boulder")
-index <- as.character(table1[['statename']])
+# index <- c("Alabama")
+
+index_smallstate <- as.character(table1_smallstate[['statename']])
 
 # create a data frame with parameters and output file names
-runs <- tibble(
-  filename = str_c(index, ".pdf"),             # creates a string with output file names in the form <index>.pdf
-  params = map(index, ~list(parameter1 = .)))  # creates a nest list of parameters for each object in the index
+runs_smallstate <- tibble(
+  filename = str_c(index_smallstate, ".pdf"),             # creates a string with output file names in the form <index>.pdf
+  params = map(index_smallstate, ~list(parameter1 = .)))  # creates a nest list of parameters for each object in the index
 
 # iterate render() along the tibble of parameters and file names
-runs %>%
+runs_smallstate %>%
   select(output_file = filename, params) %>%
-  pwalk(rmarkdown::render, input = "simple-factsheet.Rmd", output_dir = "factsheets")
+  pwalk(rmarkdown::render, input = "simple-factsheet-smallstate.Rmd", output_dir = "factsheets")
+
+# create an index
+# index <- c("Alabama")
+index_largestate <- as.character(table1_largestate[['statename']])
+
+# create a data frame with parameters and output file names
+runs_largestate <- tibble(
+  filename = str_c(index_largestate, ".pdf"),             # creates a string with output file names in the form <index>.pdf
+  params = map(index_largestate, ~list(parameter1 = .)))  # creates a nest list of parameters for each object in the index
+
+# iterate render() along the tibble of parameters and file names
+runs_largestate %>%
+  select(output_file = filename, params) %>%
+  pwalk(rmarkdown::render, input = "simple-factsheet-largestate.Rmd", output_dir = "factsheets")
