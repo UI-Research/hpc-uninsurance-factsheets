bea265abe5fb4988ff42a3a0607962a566d2824e
Menezes <mmenezes@aharris-macbookpro.urban.org>
2019-06-18 17:07:45 -0400

    fixed typos in text edits, modified condition for figure 2 note, added extra variable in data csv, printed that variable in Looking Ahead for nonexpansion states

--- iterate.R
+++ iterate.R
@@ -1,12 +1,21 @@
 library(rmarkdown)
 library(stringr)
+library(scales)
+library(ggforce)
+library(forcats)
+library(stringr)
+library(ggrepel)
 library(tidyverse)
+library(knitr)
+library(kableExtra)
+library(urbnthemes)
 
 table1 <- read.csv(file="Tables/StateFactSheetData_521.csv", header=TRUE, sep=",")
+table1[2:53, 2:84]  <- as.numeric(table1[2:53, 2:84])
 datdict <- read.csv(file="Tables/State Uninsurance WRA Data Dictionary_227.csv", header=TRUE, sep=",")
 
 index <- as.character(table1[['statename']])
-#index <- as.character("Connecticut")
+#index <- as.character("Wisconsin")
 
 # create a data frame with parameters and output file names
 runs <- tibble(
